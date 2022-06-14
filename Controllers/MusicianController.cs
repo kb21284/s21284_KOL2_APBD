e9d6d56629e93a3eba756f68c53ec70564fda6ce
kb21284 <s21284@pjwstk.edu.pl>
2022-06-14 11:47:00 +0200

    fixed AlbumController

---
+++ Controllers/MusicianController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace s21284_KOL2_APBD.Controllers
+{
+    public class MusicianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
